365a332a3ffbda0eca96d8a7597843c3702b6284
Airton Ponce <airtonponce@outlook.com>
2020-02-18 15:22:27 -0400

    include tailrec factorial

--- src/lectures/Recursion.scala
+++ src/lectures/Recursion.scala
@@ -1,15 +1,18 @@
 package lectures
 
+import scala.annotation.tailrec
+
 object Recursion extends App {
 
-  def factorial(n: Int): Int ={
-    def factorialHelper(x:Int, accumulator:Int): Int = {
+  def factorial(n: Int): BigInt = {
+    @tailrec
+    def factorialHelper(x: Int, accumulator: BigInt): BigInt = {
       if (x <= 1) accumulator
-      else factorialHelper(n-1, x * accumulator)
+      else factorialHelper(x-1, x * accumulator)
     }
     factorialHelper(n,1)
   }
 
-  println(factorial(10))
+  println(factorial(100000))
 }
 
