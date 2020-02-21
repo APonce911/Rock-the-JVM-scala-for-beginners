32fb3e3c0030ee92c28d48ceb5b1a96676d3c147
Airton Ponce <airtonponce@outlook.com>
2020-02-21 08:18:10 -0400

    imprement tailrec concatenation and WIP fibonacci

--- src/lectures/Recursion.scala
+++ src/lectures/Recursion.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object Recursion extends App {
 
-  def factorial(n: Int): BigInt = {
+  def tailrecfactorial(n: Int): BigInt = {
     @tailrec
     def factorialHelper(x: Int, accumulator: BigInt): BigInt = {
       if (x <= 1) accumulator
@@ -13,6 +13,32 @@ object Recursion extends App {
     factorialHelper(n,1)
   }
 
-  println(factorial(100000))
+  println(tailrecfactorial(10))
+
+  def tailrecConcatenate(aString: String, n: Int) : String = {
+
+    def concatenateHelper(aString: String, x: Int, accumulator: String): String = {
+      if (x <= 1) accumulator
+      else concatenateHelper(aString, x-1, aString + accumulator)
+    }
+    concatenateHelper(aString, n, "")
+  }
+  println(tailrecConcatenate("Hello Scala! ",5))
+
+
+  def tailrecFibonacciNumber(n: Int):Int = {
+    def fibonacciNumberHelper(x: Int, lastFibonacci: Int, beforeLastFibonacci: Int): Int = {
+      if (x <= 2) lastFibonacci
+//      else {
+////        fibonacciNumberHelper(x - 1???, lastFibonacci + beforeLastFibonacci)
+////        beforeLastFibonacci = lastFibonacci
+////        lastFibonacci = x-1
+//      }
+      fibonacciNumberHelper(n, 1,0)
+    }
+  }
+  println(tailrecFibonacciNumber(3))
+
+  //  create isprime test function using tailrecursion
 }
 
