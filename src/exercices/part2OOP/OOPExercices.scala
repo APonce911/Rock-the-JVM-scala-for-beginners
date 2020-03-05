8d070044ae24004684560b92da797e6f5fc97448
Airton Ponce <airtonponce@outlook.com>
2020-03-05 10:45:05 -0300

    correct isWrittenBy method

--- src/exercices/part2OOP/OOPExercices.scala
+++ src/exercices/part2OOP/OOPExercices.scala
@@ -1,14 +1,6 @@
 package exercices
 
 object OOPExercices extends App {
-  //1 -  Novel & Writer classes
-  // Writer first_name, surname, year
-  // method fullName
-
-  // Novel name, year_of_release, author
-  // method authorAge : Int
-  // method isWrittenBy :
-  // method copy(new_release_year) : Novel
 
   class Writer(firstName: String , surname: String, val year: Int   ) {
     def fullName: String = s"$firstName $surname"
@@ -17,17 +9,20 @@ object OOPExercices extends App {
   val writer: Writer = new Writer("Renan","Gomes",1980)
   println(writer)
   println(writer.fullName)
+  val imposter: Writer = new Writer("Renan","Gomes",1980)
+
 
   class Novel(name: String, yearOfRelease: Int, author: Writer) {
 
-    def isWrittenBy : String = this.author.fullName
+    def isWrittenBy(author : Writer) : Boolean = (this.author == author)
     def authorAge :Int = this.yearOfRelease - this.author.year
     def copy(newYearOfRelease: Int) = new Novel (this.name, newYearOfRelease, this.author)
   }
 
   val novel: Novel = new Novel("A Grande Viagem", 1998, writer)
   println(novel)
-  println(novel.isWrittenBy)
+  println(novel.isWrittenBy(writer))
+  println(novel.isWrittenBy(imposter))
   println(novel.authorAge)
   println(novel.copy(1999))
 
