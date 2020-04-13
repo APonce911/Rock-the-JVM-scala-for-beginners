b5853ca5464e0418f096bfaaa861d247df30bd87
Airton Ponce <airtonponce@outlook.com>
2020-04-13 08:17:12 -0300

    inheritance and traits overview

--- src/lectures/part2OOP/InheritanceAndTraits.scala
+++ src/lectures/part2OOP/InheritanceAndTraits.scala
@@ -4,13 +4,14 @@ import lectures.part2OOP.Objects.Person
 
 object InheritanceAndTraits extends App {
 
-  class Person(name: String, age: Int ) {
+  class Person(val name: String,val  age: Int ) {
     def this(name: String) = this(name, 2)
     def this() = this("Foo", 2)
   }
   class Adult(name:String, age: Int, IdCard: String) extends Person(name)
   class Adult2(name:String, age: Int, IdCard: String) extends Person
-//  val adult = new Adult
-//    println(adult)
+  val adult : Person = new Adult2("carlos",30,"832989")
+    println(adult.name)
+    println(adult.age)
 
 }
