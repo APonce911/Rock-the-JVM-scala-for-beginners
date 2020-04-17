a6559ece05bfb2859d1e6c4743045fdf9c9cb6f5
Airton Ponce <airtonponce@outlook.com>
2020-04-17 08:48:56 -0300

    define methods signatures

--- src/exercices/part2OOP/MyList.scala
+++ src/exercices/part2OOP/MyList.scala
@@ -5,18 +5,24 @@ object MyList extends App {
     /*
       TO DO Methods
       MyList describe a list of integers
-      head = first element of the list
-      tail = remainder of the list
-      isEmpty = is this list empty
-      add(int) => new list with this element added
+
+      head: Int = first element of the list
+      tail: List = remainder of the list
+      isEmpty: Boolean = is this list empty
+      add(int): new List => new list with this element added
       toString => a string representation of the list
 
      */
-    def isEmpty: Boolean = this eq Nil
+    def head: Int
+    def tail: List[Int]
+    def isEmpty: Boolean
+    def add(int : Int): List[Int]
+    def toString: String
+
   }
-  class MyIntList extends MyList
-  val myIntList  = new MyIntList()
-  println(myIntList)
-  println(myIntList.isEmpty)
+//  class MyIntList extends MyList
+//  val myIntList  = new MyIntList()
+//  println(myIntList)
+//  println(myIntList.isEmpty)
 
 }
